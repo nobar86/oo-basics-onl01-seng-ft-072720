08071d55dfe10976cd41139bd3acee311ccd043e
Michael Rabon <nobar86@gmail.com>
2020-08-06 18:50:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -36,6 +36,15 @@ class Book
     
 # end
 
-
+attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
 
