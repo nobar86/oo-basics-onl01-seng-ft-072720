cc35dccfb835014a12e65c5e3177e9c61203e8d0
Michael Rabon <nobar86@gmail.com>
2020-08-06 18:54:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :brand, :color, :size, :material
   attr_reader :condition
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   def turn_page
