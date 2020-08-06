1cbe73ec14b0623885295bb3b55445eaf5da1799
Michael Rabon <nobar86@gmail.com>
2020-08-06 18:57:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,4 @@
 class Shoe
-  # attr_accessor :brand, :color, :size, :material, :condition
-  # attr_reader :cobble
- 
-  # def initialize(brand)
-  #   @brand = brand
-  # end
- 
-  # def turn_page
-  #   puts "Flipping the page...wow, you read fast!"
-  # end
   
   def initialize(brand)
     @brand = brand
